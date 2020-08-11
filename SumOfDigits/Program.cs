c7d1bc5260fdcc99efb5c01cbd5f9577bd807760
Poojitha Pancheti <Poojitha.Pancheti@DESKTOP-UK1T43N>
2020-08-11 16:05:10 +0530

    Add project files.

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        public static int SumOfDigits(int n)
+        {
+            if (n / 10 == 0)
+                return n;
+            return SumOfDigits(n / 10) + n % 10;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(SumOfDigits(n));
+
+        }
+    }
+}
